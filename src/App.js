a118ee3dbcce5aaf4bca3a4b7c6942c02defae7f
radhikaaditech389 <radhikaaditech389@gmail.com>
2025-11-12 15:49:04 +0530

    upload code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
